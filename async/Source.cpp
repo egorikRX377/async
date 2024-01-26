8c5287f8fb0c8ed3d091c6bb63662eac74a525f2
HP <HP@DESKTOP-5DLD401>
2024-01-26 20:31:00 +0300

    Работа с async. Параметры запуска. Особенности использования

--- async/Source.cpp
+++ async/Source.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include <future>
+#include <thread>
 
-int foo(int x, int y) {
-    return x + y;
+int asyncFoo(int num)
+{
+	int result1 = num + 10;
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+	std::cout << "asyncFoo has been worked asynchronously!" << std::endl;
+	return result1;
 }
 
-int main() {
-    std::future<int> result = std::async(foo, 10, 5);
-    std::cout << "Result is: " << result.get() << std::endl;
-    return 0;
+int defferedFoo(int num)
+{
+	int result1 = num + 20;
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+	std::cout << "defferedFoo has been worked in that thread!" << std::endl;
+	return result1;
+}
+
+int main()
+{
+	std::future<int> ft1 = std::async(std::launch::async, asyncFoo, 12);
+	std::future<int> ft2 = std::async(std::launch::deferred, defferedFoo, 20);
+
+	std::cout << "Res of defferedFoo: " << ft2.get() << std::endl;
+
+	std::cout << "Res of asyncFoo: " << ft1.get() << std::endl;
+
+
+	return 0;
 }
